127155ceff5b75da8aba32d708abb72e8357e8e0
jaime <garciadiazjaime@gmail.com>
2020-12-21 18:46:10 -0600

    feat: cleanup feedme

--- src/routes/feedme.js
+++ src/routes/feedme.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const cors = require('cors');
+const debug = require('debug')('app:feedme')
 
 const {
   getProfiles
@@ -6,10 +8,28 @@ const {
 
 const router = express.Router()
 
-router.post('/feedme', async (req, res) => {
-  const { first, state, lngLat: coordinates, username } = req.body
+router.get('/feedme', cors(), async (req, res) => {
+  debug(req.query.filters)
+  const filters = JSON.parse(req.query.filters)
 
-  const profiles =  await getProfiles({ first, state, coordinates, username })
+  const { first, state, lngLat: coordinates, username } = filters
+
+  let profiles =  await getProfiles({ first, state, coordinates, username })
+
+  profiles = profiles.filter(item => !item.mediaUrl.includes('video'))
+
+  profiles = profiles.slice(0, 48).map(item => ({
+    address: item.address,
+    caption: item.caption,
+    gps: item.gps,
+    id: item.id,
+    keywords: item.keywords,
+    mediaUrl: item.mediaUrl,
+    phone: item.phones[0],
+    posts: item.posts,
+    title: item.title,
+    username: item.username,
+  }))
 
   res.send(profiles)
 })
