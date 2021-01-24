cea14cc24088015fac07b50121667e28bcc70863
jaime <garciadiazjaime@gmail.com>
2021-01-24 12:39:05 -0600

    feat: gmaps index

--- src/routes/gmaps-place.js
+++ src/routes/gmaps-place.js
@@ -4,6 +4,22 @@ const cors = require('cors')
 const router = express.Router();
 
 const { Place } = require('../model/gmaps-place')
+const categories = ['cafe', 'restaurant', 'bar']
+
+router.get('/gmaps-place',  cors(), async (req, res) => {
+  const promises = categories.map(category => Place.find({
+    type: category
+  }).limit(4))
+
+  const places = await Promise.all(promises)
+
+  const response = categories.map((category, index) => ({
+    category,
+    data: places[index]
+  }))
+
+  res.send(response)
+});
 
 router.get('/gmaps-place/:category',  cors(), async (req, res) => {
   const { category } = req.params
